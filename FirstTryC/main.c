3c1d0eb8867f568e2bbfcfccd43d105601c99d12
dreamer3c <2978425474@qq.com>
2017-06-06 09:54:07 +0800

    Initial Commit

---
+++ FirstTryC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FirstTryC
+//
+//  Created by 20161104579 on 2017/6/6.
+//  Copyright © 2017年 20161104579. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
